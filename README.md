5cdbb07978e653eefe86f24e317a910f3d1d7723
robertcollier4 <robertcollier4@yahoo.com>
2013-06-09 05:10:54 +0530

    Added Tidy command

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 AutoItScript - Syntax Package for Sublime Text 2/3
 ============
 
-AutoItScript AU3 language package for SublimeText including syntax highlighting, comments toggling, snippets, and build system.
+AutoItScript AU3 language package for SublimeText including syntax highlighting, comments toggling, snippets, build system, and Tidy command.
 
-For the build system, you will need to set your specific path to AutoIt3.exe in the file AutoIt.sublime-settings (you can open this file via the Menu Preferences>Package Settings>AutoIt Settings).
+For the build system and Tidy command, you will need to set your specific path to AutoIt3.exe and Tidy.exe in the file AutoIt.sublime-settings (you can open this file via the Menu Preferences>Package Settings>AutoIt Settings).
 
 Credits
 ------------
--- autoitbuild.py
+++ autoitbuild.py
@@ -1,10 +1,23 @@
 import sublime, sublime_plugin
+import subprocess
 
 # The autoitbuild command is called as target by AutoIt.sublime-build
 class autoitbuild(sublime_plugin.WindowCommand):
 
 	def run(self):
 		filepath = self.window.active_view().file_name()
-		ahkpath = sublime.load_settings("AutoIt.sublime-settings").get("AutoItEXEPath")
-		cmd = [ahkpath, "/ErrorStdOut", filepath]
+		AutoItExePath = sublime.load_settings("AutoIt.sublime-settings").get("AutoItExePath")
+		cmd = [AutoItExePath, "/ErrorStdOut", filepath]
 		self.window.run_command("exec", {"cmd": cmd})
+
+class autoittidy(sublime_plugin.WindowCommand):
+
+	def run(self):
+		self.window.run_command("save")
+		filepath = self.window.active_view().file_name()
+		TidyExePath = sublime.load_settings("AutoIt.sublime-settings").get("TidyExePath")
+		tidycmd = [TidyExePath, filepath]
+		tidyprocess = subprocess.Popen(tidycmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+		tidyoutput = tidyprocess.communicate()[0].rstrip()
+		self.window.run_command("revert")
+		sublime.status_message(tidyoutput)
